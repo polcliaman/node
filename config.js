f8d4c284816aafada29edb0e5b2a58c2e2835274
polcliaman <47861721+polcliaman@users.noreply.github.com>
2019-03-15 14:50:51 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
 // Set mongodb connection string here
-module.exports.MONGO_CONNECTION_STRING = 'mongodb://node-shop-admin:node-shop-admin@node-shop-shard-00-00-o0mrt.mongodb.net:27017,node-shop-shard-00-01-o0mrt.mongodb.net:27017,node-shop-shard-00-02-o0mrt.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-shard-0&authSource=admin&retryWrites=true';
+module.exports.MONGO_CONNECTION_STRING = 'mongodb://USER:PWD@CLUSTER';
